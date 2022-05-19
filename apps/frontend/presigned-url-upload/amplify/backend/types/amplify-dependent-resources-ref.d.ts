9c373ac660b705c64615b9ceebfb8c1388666066
hustshawn <hustshawn@gmail.com>
2022-05-19 16:51:51 +0800

    Enabled both dev and prod env

--- apps/frontend/presigned-url-upload/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ apps/frontend/presigned-url-upload/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "ssademofrontend9cdd6fa2": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
