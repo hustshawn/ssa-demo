0e7a0af1f7421d83aa068893752f553fddbda779
hustshawn <hustshawn@gmail.com>
2022-05-19 12:20:02 +0800

    Updated the VPC setup

--- infra/vpc/main.tf
+++ infra/vpc/main.tf
@@ -16,6 +16,10 @@ module "vpc" {
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
   enable_nat_gateway = true
   enable_vpn_gateway = false
   single_nat_gateway = true
